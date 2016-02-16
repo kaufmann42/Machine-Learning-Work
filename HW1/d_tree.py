31c4ecf628d51171da8479bc35f1aa0ee023cb27
kaufmann42 <kaufmann43@gmail.com>
2016-02-16 10:21:14 -0500

    starting HW2

--- HW1/d_tree.py
+++ HW1/d_tree.py
@@ -2,11 +2,12 @@ from sklearn import tree
 import pandas as pd
 
 mydata = pd.read_csv('data.csv')
-target = mydata["att1"]  #provided your csv has header row, and the label column is named "Label"
+target = mydata["att1"]  #classification column
 
 #select all but the last column as data
 data = mydata.ix[:,:-1]
 
+#train tree
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(data, target)
 
@@ -19,6 +20,7 @@ two = 0
 three = 0
 four = 0
 
+#construct confusion matrix
 for i in range(0,total):
     if predicted[i] == 0 and actual[i] == 0:
         one += 1
@@ -28,6 +30,7 @@ for i in range(0,total):
         three += 1
     elif predicted[i] == 1 and actual[i] == 1:
         four += 1
+#print everythin
 print("***Decision Tree***")
 print("\t\t"+"Predicted Negative\t"+"Predicted Positive\t")
 print("Negative Cases\t"+str(one)+"\t\t\t"+str(two)+"\t")
@@ -38,6 +41,7 @@ print("The recall was: "+ "%.2f"%float(four/(four+three)))
 print("The precision was: "+ "%.2f"%float(four/(four+two)))
 
 from sklearn.neighbors import KNeighborsClassifier
+#train knn model
 neigh = KNeighborsClassifier()
 neigh.fit(data, target)
 
@@ -50,6 +54,7 @@ two = 0
 three = 0
 four = 0
 
+#construct confusion matrix
 for i in range(0,total):
     if predicted[i] == 0 and actual[i] == 0:
         one += 1
@@ -59,6 +64,7 @@ for i in range(0,total):
         three += 1
     elif predicted[i] == 1 and actual[i] == 1:
         four += 1
+#print confusion matrix and Statistics
 print("\n***kNN***")
 print("\t\t"+"Predicted Negative\t"+"Predicted Positive\t")
 print("Negative Cases\t"+str(one)+"\t\t\t"+str(two)+"\t")
