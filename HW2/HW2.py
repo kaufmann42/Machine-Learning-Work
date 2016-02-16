3c6fa5b8fad2c032d5e6ea78702d7ee524c8f62b
kaufmann42 <kaufmann43@gmail.com>
2016-02-16 16:03:57 -0500

    re-committing and re-starting

---
+++ HW2/HW2.py
@@ -0,0 +1,9 @@
+#import statements
+import SKLearn as svc
+import numpy as np
+import pandas as pd
+
+#import the database
+pd.read_excel('Data_Cortex_Nuclear.xlsx', 'Sheet1', index_col=None, na_values=['NA'])
+
+# svc = svm.SVC(kernal = 'linear')
